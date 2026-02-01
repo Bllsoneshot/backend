dcfe8354ebb261eb5499fd0036270a5b16495c3b
Kim, Joon <kjoon418@naver.com>
2026-02-01 16:38:48 +0900

    feat: 프로젝트 세팅

---
+++ src/main/kotlin/goodspace/bllsoneshot/BllsoneshotApplication.kt
@@ -0,0 +1,11 @@
+package goodspace.bllsoneshot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BllsoneshotApplication
+
+fun main(args: Array<String>) {
+    runApplication<BllsoneshotApplication>(*args)
+}
