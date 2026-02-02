8fcd19ddc8667a22023bc77b139702bf52c3cde7
TwooTwoo <mbnman12@gmail.com>
2026-02-03 00:04:33 +0900

    feat(file): 파일 업로드 모듈 구현

--- src/main/kotlin/goodspace/bllsoneshot/global/file/FileStorageService.kt
+++ src/main/kotlin/goodspace/bllsoneshot/global/file/FileStorageService.kt
@@ -1,19 +1,48 @@
 package goodspace.bllsoneshot.global.file
 
+import goodspace.bllsoneshot.entity.assignment.File
+import goodspace.bllsoneshot.global.file.dto.FileUploadResponse
+import goodspace.bllsoneshot.repository.file.FileRepository
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.multipart.MultipartFile
 import software.amazon.awssdk.core.sync.RequestBody
 import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest
 import software.amazon.awssdk.services.s3.model.PutObjectRequest
 import java.util.UUID
 
 @Service
 class FileStorageService(
     private val s3Client: S3Client,
-    @Value("\${aws.s3.bucket}") private val bucket: String
+    private val fileRepository: FileRepository,
+    @Value("\${aws.s3.bucket}") private val bucket: String,
+    @Value("\${aws.s3.cdn-url}") private val cdnUrl: String
 ) {
-    fun uploadWorksheet(file: MultipartFile): UploadedFile {
+    @Transactional
+    fun uploadWorksheet(file: MultipartFile): FileUploadResponse {
+        val uploaded = uploadToS3(file)
+
+        try {
+            val savedFile = fileRepository.save(
+                File(
+                    fileName = uploaded.fileName,
+                    contentType = uploaded.contentType,
+                    byteSize = uploaded.byteSize,
+                    bucketName = uploaded.bucketName,
+                    objectKey = uploaded.objectKey
+                )
+            )
+            val url = "${cdnUrl.trimEnd('/')}/${uploaded.objectKey}"
+            return FileUploadResponse(savedFile.id!!, url, savedFile.fileName, savedFile.contentType)
+        } catch (e: Exception) {
+            deleteFromS3(uploaded.objectKey)
+            throw e
+        }
+    }
+
+    private fun uploadToS3(file: MultipartFile): UploadedFile {
         val objectKey = "worksheets/${UUID.randomUUID()}_${file.originalFilename}"
         val request = PutObjectRequest.builder()
             .bucket(bucket)
@@ -31,6 +60,15 @@ class FileStorageService(
             byteSize = file.size,
             bucketName = bucket
         )
+    }
+
+    private fun deleteFromS3(objectKey: String) {
+        val request = DeleteObjectRequest.builder()
+            .bucket(bucket)
+            .key(objectKey)
+            .build()
+
+        s3Client.deleteObject(request)
     }
 }
 
